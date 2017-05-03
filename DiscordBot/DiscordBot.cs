f72e3e7c10628f761d8d7ba972168f5bf6b80d29
leonardo carlos gomes <leonardo35671@hotmail.com>
2017-05-03 04:03:08 -0300

    Mensagem via tts

---
+++ DiscordBot/DiscordBot.cs
@@ -0,0 +1,49 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    public class DiscordBot
+    {
+        DiscordClient client;
+        CommandService commands;
+
+        public DiscordBot()
+        {
+            commands = client.GetService<CommandService>();
+            client = new DiscordClient(input =>
+            {
+                input.LogLevel = LogSeverity.Info;
+                input.LogHandler = Log;
+            });
+
+            client.UsingCommands(input =>
+            {
+                input.PrefixChar("/");
+                input.AllowMentionPrefix(true);
+            }
+            );
+
+            commands.CreateCommand("teste").Do(async (e) =>          
+            {
+                await e.Channel.SendTTSMessage("Testando o bot putos");
+            });
+
+            client.ExecuteAndWait(async() =>
+            {
+                await client.Connect("MzA5MTU0MTYxMzA2NTAxMTIw.C-rRyA.j7qCtBcLeaO6QHj7YIlPU2nqIy8", TokenType.Bot);
+            }
+            );
+        }
+
+        private void Log(object sender, LogMessageEventArgs e) {
+            Console.WriteLine(e.Message);
+
+        }
+    }
+}
--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -10,6 +10,7 @@ namespace DiscordBot
     {
         static void Main(string[] args)
         {
+            DiscordBot bot = new DiscordBot();
         }
     }
 }
